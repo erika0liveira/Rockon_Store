74ca41812b6a7d454d47f00b48b3afe08c84110d
Lucas de Paula Oliveira Santos <lkslukas23@gmail.com>
2021-11-09 01:30:36 -0300

    Criação do header e index

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import index, item
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('index.html', views.index, name='index'),
-    path('item.html', views.item, name='item')
+    path('', index, name='index'),
+    path('index.html', index, name='index'),
+    path('item.html', item, name='item'),
+    # path('', teste_produto, name='index'),
 ]
