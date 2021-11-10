3a285e5b894f3084699e60ed9ac6ce20147247c2
Lucas de Paula Oliveira Santos <lkslukas23@gmail.com>
2021-11-10 01:59:23 -0300

    Aplicação de estilo em index e item

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 from .models import Produto
 
@@ -8,6 +11,13 @@ def index(request):
     return render(request, "index.html", context)
 
 
-def item(request):
-    return render(request, 'item.html')
+def item(request, pk):
+    teste = {'teste': Produto.objects.get(id=pk)}
+    return render(request, 'item.html', teste)
 
+class Teste(generic.DetailView):
+    model = Produto
+    template_name = 'item.html'
+    def get_queryset(self):
+        return Produto.objects.all()
+    
