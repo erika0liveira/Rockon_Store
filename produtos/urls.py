5245b31db2b46938825e3bdae9021af7d4188f58
Erika <Erikaalves018@gmail.com>
2021-11-26 01:00:56 -0300

    implementação de busca

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, item
+from .views import index, item, buscar
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('index', index, name='index'),
+    path('buscar', buscar, name='buscar'),
     path('<int:pk>/item', item, name='item'),
 ]
 
--- produtos/views.py
+++ produtos/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views import generic
-
 from .models import Produto
 
 def index(request):
@@ -13,3 +9,21 @@ def index(request):
 def item(request, pk):
     context = {'item': Produto.objects.get(id=pk)}
     return render(request, 'item.html', context)
+
+
+def buscar(request):
+    busca_produto = Produto.objects.order_by('-preco').filter(disponivel=True)
+    print(request.GET)
+
+    if 'buscar' in request.GET:
+        nome_buscar = request.GET['buscar']
+        if buscar:
+            busca_produto = busca_produto.filter(nome_produto__icontains=nome_buscar)
+            
+            
+   
+    dados = {
+        'produtos': busca_produto,
+    }
+
+    return render(request, 'buscar.html', dados)
